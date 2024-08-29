d8712bd68898a6c7723576cdd0aa4b63bac575d8
Vicenç Garcia <me@vgaltes.com>
2024-08-29 09:07:15 +0200

    style

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home({ params: { locale } }: Props) {
   return (
     <PageLayout>
       <h2 className="text-xl font-semibold mb-6">{translate("title")}</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6">
         {products.map((product) => (
           <div
             key={product.id}
